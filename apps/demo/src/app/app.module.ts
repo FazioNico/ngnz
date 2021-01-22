fb332bb866d6e7abcb5c111f9da210e096f2b858
FazioNico <contact@nicolasfazio.ch>
2021-01-22 16:02:36 +0100

    upd: md

--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CopyToClipboardModule } from '@ngnz/copy-to-clipboard';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    CopyToClipboardModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
