fa2391c5e3462d0a388f45c0ed2edf159857679e
FazioNico <contact@nicolasfazio.ch>
2021-01-22 14:31:10 +0100

    upd(ctc): export directives

--- libs/copy-to-clipboard/src/lib/copy-to-clipboard.module.ts
+++ libs/copy-to-clipboard/src/lib/copy-to-clipboard.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CopyToClipboardDirective } from './directives/copy-to-clipboard/copy-to-clipboard.directive';
+
+const DIRECTIVES = [CopyToClipboardDirective];
 @NgModule({
   imports: [CommonModule],
-  declarations: [CopyToClipboardDirective],
+  declarations: [
+    ...DIRECTIVES
+  ],
+  exports: [
+    ...DIRECTIVES
+  ]
 })
 export class CopyToClipboardModule {}
