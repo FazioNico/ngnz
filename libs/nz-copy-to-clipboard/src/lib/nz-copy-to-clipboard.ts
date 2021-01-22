2c8cc02c265dd58a14f8d0d1634dbad4910b3b85
FazioNico <contact@nicolasfazio.ch>
2021-01-22 15:27:44 +0100

    upd(ctc): clean interface

--- libs/nz-copy-to-clipboard/src/lib/nz-copy-to-clipboard.ts
+++ libs/nz-copy-to-clipboard/src/lib/nz-copy-to-clipboard.ts
@@ -2,18 +2,11 @@
 import * as copy from 'copy-to-clipboard';
 
 export interface NzCopyToClopboardOptions {
-  debug?: boolean;
-  message?: string;
-  format?: string; // MIME type
   handler?: (text: string) => void;
 }
 
-export const DEFAULT_OPTIONS: NzCopyToClopboardOptions = {
-  debug: false,
-}
-
 export const nzCopyToClipboard = (text: string, ops: NzCopyToClopboardOptions) => {
-  const result = copy(text, {...DEFAULT_OPTIONS, ...ops});
+  const result = copy(text);
   if (result && !ops?.handler) {
     alert('copied!');
   }
