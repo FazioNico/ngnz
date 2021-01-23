aaa0ecea7b399825ed3e31832f653e867f7f7855
FazioNico <contact@nicolasfazio.ch>
2021-01-23 12:12:17 +0100

    upd(demo): add @ngnz/dayjs with module config settup

--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -2,15 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+// import for @ngnz/copy-to-clipboard
 import { CopyToClipboardModule } from '@ngnz/copy-to-clipboard';
+// import for @ngnz/dayjs
+import { DayjsModule, DAYJS_LOCAL_PRESSET } from '@ngnz/dayjs';
+import 'dayjs/locale/en'; // import desired language
+// config for @ngnz/dayjs
+const localDayjsPressetFactory = () => {
+  // add logic to get user selected language
+  return 'en'
+};
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    CopyToClipboardModule
+    CopyToClipboardModule,
+    DayjsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: DAYJS_LOCAL_PRESSET,
+      useFactory() {
+        return localDayjsPressetFactory()
+      }
+    }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
