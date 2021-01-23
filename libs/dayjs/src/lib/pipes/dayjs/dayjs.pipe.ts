c7cec4b10189a5c496b6b08ee396976c75fedf7f
FazioNico <contact@nicolasfazio.ch>
2021-01-23 12:27:12 +0100

    upd(dayjs): fix incorrect type implemeentation

--- libs/dayjs/src/lib/pipes/dayjs/dayjs.pipe.ts
+++ libs/dayjs/src/lib/pipes/dayjs/dayjs.pipe.ts
@@ -1,7 +1,6 @@
 import { Inject, Optional, Pipe, PipeTransform } from '@angular/core';
-import { NzDayJSPipe } from '@ngnz/core'
+import { NzDayJSPipe, DayJsPipeResponse } from '@ngnz/core'
 import { DAYJS_LOCAL_PRESSET, DayjsPresset } from '../../token';
-
 const DEFAULT_LOCAL_PRESSET = 'fr';
 
 @Pipe({
@@ -11,6 +10,16 @@ export class DayJSPipe extends NzDayJSPipe implements PipeTransform {
 
   constructor(@Optional() @Inject(DAYJS_LOCAL_PRESSET) localpresset?: DayjsPresset) {
     super(localpresset||DEFAULT_LOCAL_PRESSET);
+  }
+
+  transform(
+    value: string, 
+    method: 'format' | 'fromNow' | 'to' | 'isAfter', 
+    ops: any, 
+    bool?: boolean
+  ): DayJsPipeResponse {
+    const res = super.transform(value, method, ops, bool);
+    return res;
   }
 
 }
