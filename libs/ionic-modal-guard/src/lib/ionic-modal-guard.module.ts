99b70bd8fdea0c3c802a03aff1edbaf02ca15b5b
FazioNico <contact@nicolasfazio.ch>
2021-01-25 23:35:51 +0100

    upd(ImG): rmv angular module export

--- libs/ionic-modal-guard/src/lib/ionic-modal-guard.module.ts
+++ libs/ionic-modal-guard/src/lib/ionic-modal-guard.module.ts
@@ -1,11 +1,2 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { IonicModalGuard } from './guard/ionic-modal.guard';
-
 export * from './guard/ionic-modal.guard';
-@NgModule({
-  imports: [CommonModule],
-  providers: [IonicModalGuard],
-  exports: [IonicModalGuard]
-})
-export class IonicModalGuardModule {}
+
