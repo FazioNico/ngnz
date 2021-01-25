f0ca459e61d409deebf01919bedc8b16e5fa847d
FazioNico <contact@nicolasfazio.ch>
2021-01-25 21:50:08 +0100

    upd(ctc): rmv dependencies

--- libs/nz-copy-to-clipboard/src/lib/nz-copy-to-clipboard.ts
+++ libs/nz-copy-to-clipboard/src/lib/nz-copy-to-clipboard.ts
@@ -1,12 +1,20 @@
-
-import * as copy from 'copy-to-clipboard';
-
 export interface NzCopyToClopboardOptions {
   handler?: (text: string) => void;
 }
 
-export const nzCopyToClipboard = (text: string, ops: NzCopyToClopboardOptions) => {
-  const result = copy(text);
+const _ngnz_copy = async (text: string) => {
+  // use native js window function to copy text to clipboard
+  if (!navigator?.clipboard) {
+    throw '[ERROR] navigator.clipboard not supprot in browser'
+  }
+  return navigator.clipboard
+    .writeText(text)
+    .then(() => true)
+    .catch(() => false);
+};
+
+export const nzCopyToClipboard = async (text: string, ops: NzCopyToClopboardOptions) => {
+  const result = await _ngnz_copy(text);
   if (result && !ops?.handler) {
     alert('copied!');
   }
