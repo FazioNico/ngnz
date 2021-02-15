89fd4eda97581e2ee76bdb66ddd3e89359a15f8f
FazioNico <contact@nicolasfazio.ch>
2021-02-15 23:11:37 +0100

    upd(demo): testing download

--- apps/demo/src/app/components/index-page/index-page.component.ts
+++ apps/demo/src/app/components/index-page/index-page.component.ts
@@ -5,11 +5,34 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './index-page.component.html',
   styleUrls: ['./index-page.component.scss']
 })
-export class IndexPageComponent implements OnInit {
+export class IndexPageComponent {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  download() {
+    const blob = new Blob(['{"name": "test"}']);
+    const blobUrl = URL.createObjectURL(blob);
+
+    // Create a link element
+    const link = document.createElement('a');
+  
+    // Set link's href to point to the Blob URL
+    link.href = blobUrl;
+    (link.download as any) = 'myfile.json';
+  
+    // Append link to the body
+    document.body.appendChild(link);
+
+    // Dispatch click event on the link
+    // This is necessary as link.click() does not work on the latest firefox
+    link.dispatchEvent(
+      new MouseEvent('click', { 
+        bubbles: true, 
+        cancelable: true, 
+        view: window 
+      })
+    );
+  
+    // Remove link from body
+    document.body.removeChild(link);
   }
-
 }
